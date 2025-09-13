d44024470644a424e17faf821157366e6cc7c931
Lery0816 <160610040+Lery0816@users.noreply.github.com>
2025-09-13 14:17:20 +1000

    85%-Movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 2.0f;
+    private int currentTarget = 0;
+    private Vector2[] pathPoints;
+    private Animator animator;
+    private AudioSource moveSFX;
+    [SerializeField] public AudioClip moveClip;  
+    
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        moveSFX = GetComponent<AudioSource>();
+        pathPoints = new Vector2[]
+        {
+            new Vector2(1f, -1f),
+            new Vector2(6f, -1f),
+            new Vector2(6f, -5f),
+            new Vector2(1f, -5f)
+        };
+        
+        transform.position = pathPoints[0];
+    }
+    
+    void Update()
+    {
+        if (pathPoints.Length == 0) return;
+        
+        Vector2 target = pathPoints[currentTarget];
+        
+        transform.position = Vector2.MoveTowards(transform.position, target, moveSpeed * Time.deltaTime);
+        
+        Vector2 dir = target - (Vector2)transform.position;
+
+        if (Mathf.Abs(dir.x) > Mathf.Abs(dir.y))
+        {
+            if (dir.x > 0) animator.Play("player-right");
+            else animator.Play("player-left");
+        }
+        else
+        {
+            if (dir.y > 0) animator.Play("player-up");
+            else animator.Play("player-down");
+        }
+        
+        if (Vector2.Distance(transform.position, target) < 0.01f)
+        {
+            currentTarget = (currentTarget + 1) % pathPoints.Length;
+        }
+        
+        if (dir.magnitude > 0.01f)
+        {
+            if (!moveSFX.isPlaying)
+            {
+                moveSFX.clip = moveClip;
+                moveSFX.loop = true;
+                moveSFX.Play();
+            }
+        }
+        else
+        {
+            if (moveSFX.isPlaying)
+            {
+                moveSFX.Stop();
+            }
+        }
+    }
+}
